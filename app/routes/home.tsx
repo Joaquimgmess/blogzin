ecef968b2621ce4e964cf3afa1d59b19fe4eecbc
Joaquimgmess <joaquimgmess21@gmail.com>
2025-06-02 18:45:20 -0300

    Add Prisma configuration, integrate database client, and implement post fetching in home route

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,3 +1,6 @@
+import prisma from "prisma/prisma";
+import { useLoaderData } from "react-router";
+import type { Post } from "~/generated/prisma";
 import type { Route } from "./+types/home";
 
 export function meta({}: Route.MetaArgs) {
@@ -7,6 +10,24 @@ export function meta({}: Route.MetaArgs) {
   ];
 }
 
+export async function loader() {
+  const posts = await prisma.post.findMany();
+  return { posts };
+}
+
 export default function Home() {
-  return <div>Home</div>;
+  const { posts } = useLoaderData<typeof loader>();
+  return (
+    <div>
+      <h1>Posts</h1>
+      {posts.map((post: Post) => (
+      <div key={post.id}>
+        <h1>{post.title}</h1>
+        <p>{post.originalText}</p>
+        <p>{post.source}</p>
+          <p>{post.createdAt.toLocaleDateString()}</p>
+        </div>
+      ))}
+    </div>
+  );
 }
